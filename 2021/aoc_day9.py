ef146330d267428bf8f9b356eb87ddb048828e72
plasmoidia <plasmoidia@gmail.com>
2021-12-09 21:54:03 -0600

    Add solution for day9 part 2

--- 2021/aoc_day9.py
+++ 2021/aoc_day9.py
@@ -2,6 +2,25 @@
 
 import numpy as np
 
+def flood_fill(img, start, count=0):
+    up = (start[0]-1, start[1])
+    down = (start[0]+1, start[1])
+    left = (start[0], start[1]-1)
+    right = (start[0], start[1]+1)
+    img[start] = False
+    count += 1
+    if not img[up] and not img[down] and not img[left] and not img[right]:
+        return count
+    if img[up]:
+        count = flood_fill(img, up, count)
+    if img[down]:
+        count = flood_fill(img, down, count)
+    if img[left]:
+        count = flood_fill(img, left, count)
+    if img[right]:
+        count = flood_fill(img, right, count)
+    return count
+
 if __name__ == '__main__':
     filename = 'aoc_day9_input.txt'
     height = np.genfromtxt(filename, delimiter=1, dtype=int)
@@ -21,3 +40,21 @@ if __name__ == '__main__':
     total = np.sum(minima) + num
 
     print(f'Found {num} minima with total {total} risk')
+
+    sz = padded.shape
+    rows = np.repeat([np.arange(sz[0], dtype=int)], sz[1], axis=0).transpose()
+    cols = np.repeat([np.arange(sz[1], dtype=int)], sz[0], axis=0)
+    minima_pts = zip(rows[minima_loc], cols[minima_loc])
+
+    basins = padded < 9
+    basin_szs = np.zeros(num, dtype=int)
+    for basin, pt in enumerate(minima_pts):
+        basin_szs[basin] = flood_fill(basins, pt)
+
+    basin_szs.sort()
+    big_three = basin_szs[-3:]
+    sz_str = ', '.join([str(s) for s in big_three])
+    product = np.prod(big_three)
+
+    print(f'Found largest three basins {sz_str} with product {product}')
+
