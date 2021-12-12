894314ce09d2125c385a6bc3f28954c7c6694ee3
plasmoidia <plasmoidia@gmail.com>
2021-12-12 15:53:18 -0600

    Update day12 for part 2 solution

--- 2021/aoc_day12.py
+++ 2021/aoc_day12.py
@@ -16,23 +16,43 @@ def can_visit(path, node):
     small = small_cave(node)
     if not small:
         res = True
-    elif node not in path:
+    elif node == 'end':
         res = True
+    elif node == 'start':
+        res = False
+    else: # small
+        if path['can_visit']:
+            if node in path['list']:
+                path['can_visit'] = False
+            res = True
+        else:
+            if node not in path['list']:
+                res = True
+    return res
+
+def double_small_cave(path):
+    res = False
+    for node in path:
+        if small_cave(node) and path.count(node) > 1:
+            res = True
+            break
     return res
 
 def find_path(paths, cave, path, start, end):
-    path.append(start)
+    path['list'].append(start)
     if start == end:
-        paths.append(cp.deepcopy(path))
+        paths.append(cp.deepcopy(path['list']))
     else:
         for node in cave[start]:
             if can_visit(path, node):
                 find_path(paths, cave, path, node, end)
-    path.pop()
+    path['list'].pop()
+    if path['can_visit'] is not None:
+        path['can_visit'] = not double_small_cave(path['list'])
 
-def find_paths(cave, start='start', end='end'):
+def find_paths(cave, start='start', end='end', can_visit=None):
     paths = []
-    path = []
+    path = {'can_visit': can_visit, 'list': []}
     find_path(paths, cave, path, start, end)
     return paths
 
@@ -51,3 +71,7 @@ if __name__ == '__main__':
     paths = find_paths(cave)
     num = len(paths)
     print(f'Found {num} paths')
+
+    paths = find_paths(cave, can_visit=True)
+    num = len(paths)
+    print(f'Found {num} paths visiting 1 small cave twice')
